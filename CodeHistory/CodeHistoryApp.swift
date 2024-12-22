41001fa63cdcd875c1e89ec1a89a0c4275efb5cb
inewins <anh.nguyen2@mavs.uta.edu>
2024-12-22 17:30:08 -0600

    Initial Commit

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Anh Nguyen on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
